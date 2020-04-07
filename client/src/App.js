9dbc342c7aca4473439f6cf2b35869acf3f3a35c
JuanIrache <yrache@gmail.com>
2020-04-07 22:45:48 +0200

    init react app

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Nebots</h1>
+      </header>
+      <div className="chooser">
+        <div className="users">Necessito ajuda</div>
+        <div className="providers">Vull ajudar</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
