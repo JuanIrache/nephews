58e29d6655928bf3e65edab39dc3993840f4098c
JuanIrache <yrache@gmail.com>
2020-04-07 23:29:15 +0200

    add first stylesheet and basic home html

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import './style/App.scss';
 
 function App() {
   return (
     <div className="App">
       <header>
-        <h1>Nebots</h1>
+        <h1>nebots</h1>
       </header>
       <div className="chooser">
-        <div className="users">Necessito ajuda</div>
-        <div className="providers">Vull ajudar</div>
+        <div className="clients">
+          <p>Necessito ajuda</p>
+        </div>
+        <div className="providers">
+          <p>Vull ajudar</p>
+        </div>
       </div>
     </div>
   );
