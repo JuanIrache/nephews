b43512a0372b55085a360e518fade8ee5606d6cc
JuanIrache <yrache@gmail.com>
2020-04-08 13:18:13 +0200

    save providers to database

--- client/src/components/Providers.js
+++ client/src/components/Providers.js
@@ -1,35 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import '../style/Providers.scss';
 
-export default () => (
-  <div className="Providers">
-    <form>
-      <h3>Registra el teu telèfon per ajudar a qui necessiti suport tècnic</h3>
-      <label htmlFor="name" className="small">
-        Nom
-      </label>
-      <input type="text" name="name" id="name" required />
+export default () => {
+  const [form, setForm] = useState({ name: '', lastName: '', phone: '' });
+  const [result, setResult] = useState('');
+  const onChange = e => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  const onSubmit = e => {
+    e.preventDefault();
+    fetch(process.env.REACT_APP_SERVER + '/provider', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(form)
+    })
+      .then(res => {
+        if (res.status == 200) setResult('success');
+        else setResult('fail');
+      })
+      .catch(err => {
+        console.error(err);
+        setResult('fail');
+      });
+  };
+  return (
+    <div className="Providers">
+      {result != 'success' ? (
+        <form onSubmit={onSubmit}>
+          <h3>
+            Registra el teu telèfon per ajudar a qui necessiti suport tècnic
+          </h3>
+          <label htmlFor="name" className="small">
+            Nom
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            value={form.name}
+            onChange={onChange}
+            required
+          />
 
-      <label htmlFor="lastName" className="small">
-        Cognoms
-      </label>
-      <input type="text" name="lastName" id="lastName" required />
+          <label htmlFor="lastName" className="small">
+            Cognoms
+          </label>
+          <input
+            type="text"
+            name="lastName"
+            id="lastName"
+            value={form.lastName}
+            onChange={onChange}
+            required
+          />
 
-      <label htmlFor="phone" className="small">
-        Telèfon
-      </label>
-      <input type="tel" name="phone" id="phone" required />
+          <label htmlFor="phone" className="small">
+            Telèfon mòbil
+          </label>
+          <input
+            type="tel"
+            name="phone"
+            id="phone"
+            value={form.phone}
+            onChange={onChange}
+            required
+          />
 
-      <label>
-        <input type="checkbox" name="conditions" required /> Acceptes les
-        condicions d'ús?
-      </label>
-      <input type="submit" value="Registrar" />
-    </form>
-    <Link to="/About">
-      <h4>Com funciona?</h4>
-    </Link>
-  </div>
-);
+          <label>
+            <input type="checkbox" name="conditions" required /> Acceptes les
+            condicions d'ús?
+          </label>
+          <input type="submit" value="Registrar" />
+          {result == 'fail' && (
+            <p className="warning">Error comunicant amb el servidor</p>
+          )}
+        </form>
+      ) : (
+        <p className="success">Gràcies, aviat rebràs instruccions per SMS.</p>
+      )}
+
+      <Link to="/About">
+        <h4>Com funciona?</h4>
+      </Link>
+    </div>
+  );
+};
--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const app = require('express')();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const uid = require('uid');
 
 const Provider = require('./models/Provider');
 
@@ -14,12 +15,12 @@ const {
   GTE_PREMIUM_ATLAS_USER,
   GTE_PREMIUM_ATLAS_PASS,
   GTE_PREMIUM_ATLAS_SERVER,
-  GTE_PREMIUM_ATLAS_DB,
+  GTE_PREMIUM_ATLAS_DB
 } = process.env;
 
 const getDate = () => {
   return new Date().toLocaleString('es-ES', {
-    timeZone: 'Europe/Madrid',
+    timeZone: 'Europe/Madrid'
   });
 };
 
@@ -31,7 +32,7 @@ mongoose.connect(
     useNewUrlParser: true,
     useFindAndModify: false,
     useCreateIndex: true,
-    useUnifiedTopology: true,
+    useUnifiedTopology: true
   }
 );
 
@@ -39,25 +40,40 @@ const corsOptions =
   NODE_ENV === 'production'
     ? {
         origin: GTE_PREMIUM_CLIENTURL,
-        optionsSuccessStatus: 200,
+        optionsSuccessStatus: 200
       }
-    : {};
+    : null;
 
 app.get('/provider/:id', cors(corsOptions), async (req, res) => {
   const provider = await Provider.findOne({
-    'payments.downloadKey': req.params.id,
+    'payments.downloadKey': req.params.id
   }).exec();
   return res.status(200).send('<h1>To Do</h1>');
 });
 
-app.post('/provider', cors(corsOptions), async (req, res) => {
-  if (req.body) {
-    return res.json({
-      valid: true,
-    });
+// enable pre-flight request
+app.options('/provider', cors());
+
+app.post('/provider', cors(), async (req, res) => {
+  let { name, lastName, phone } = req.body || {};
+  if (name && lastName && phone) {
+    try {
+      const validateKey = uid(12);
+      if (phone.slice(0, 3) != '+34' && phone.slice(0, 4) != '0034') {
+        phone = '+34' + phone;
+      }
+      // Delete if already existing
+      await Provider.deleteOne({ phone }).exec();
+      await Provider.create({ name, lastName, phone, validateKey });
+    } catch (error) {
+      console.log(error);
+      return res.sendStatus(500);
+    }
+
+    return res.sendStatus(200);
   }
   return res.status(400).send({
-    message: 'Bad coupon data',
+    message: 'Bad data'
   });
 });
 
