605199d947e3f23c450846e1f69269e75ccc6337
JuanIrache <yrache@gmail.com>
2020-04-09 15:07:39 +0200

    use existing env file for deploy variable

--- client/deploy.js
+++ client/deploy.js
@@ -1,12 +1,15 @@
-const { deployFolder } = require('./keys');
 const fs = require('fs-extra');
 const path = require('path');
 
-fs.remove(deployFolder, err => {
+require('dotenv').config({ path: path.resolve(process.cwd(), '.env.local') });
+
+const { REACT_APP_DEPLOY } = process.env;
+
+fs.remove(REACT_APP_DEPLOY, err => {
   if (err) return console.error(err);
   console.log('Deploy folder deleted');
 
-  fs.copy('./build', deployFolder, err => {
+  fs.copy('./build', REACT_APP_DEPLOY, err => {
     if (err) return console.error(err);
     console.log('build copied');
   }); // copies directory, even if it has subdirectories or files
