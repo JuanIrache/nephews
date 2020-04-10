f9c75892e255d1d0ea9ad006530b7fd0afd5194a
JuanIrache <yrache@gmail.com>
2020-04-10 13:20:52 +0200

    add provider languages and skills to front end

--- client/src/components/Providers.js
+++ client/src/components/Providers.js
@@ -4,11 +4,30 @@ import { Link } from 'react-router-dom';
 import '../style/Providers.scss';
 
 export default () => {
-  const [form, setForm] = useState({ name: '', lastName: '', phone: '' });
+  const [form, setForm] = useState({
+    name: '',
+    lastName: '',
+    phone: '',
+    languages: [],
+    skills: []
+  });
+
   const [result, setResult] = useState('');
+
   const onChange = e => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
+
+  const onChangeCheck = e => {
+    const group = e.target.getAttribute('data-group');
+    let arr = form[group];
+
+    if (e.target.checked) arr = [...arr, e.target.name];
+    else arr = arr.filter(a => a !== e.target.name);
+
+    setForm({ ...form, [group]: arr });
+  };
+
   const onSubmit = e => {
     e.preventDefault();
     fetch(process.env.REACT_APP_SERVER + '/provider', {
@@ -69,6 +88,122 @@ export default () => {
             onChange={onChange}
             required
           />
+
+          <div className="checkgroup">
+            <label className="small">Idiomes</label>
+            <label className="check">
+              <input
+                type="checkbox"
+                name="cat"
+                data-group="languages"
+                onChange={onChangeCheck}
+              />{' '}
+              Català
+            </label>
+            <label className="check">
+              <input
+                type="checkbox"
+                name="spa"
+                data-group="languages"
+                onChange={onChangeCheck}
+              />{' '}
+              Castellà
+            </label>
+            <label className="check">
+              <input
+                type="checkbox"
+                name="eng"
+                data-group="languages"
+                onChange={onChangeCheck}
+              />{' '}
+              Anglès
+            </label>
+            <label className="check">
+              <input
+                type="checkbox"
+                name="urd"
+                data-group="languages"
+                onChange={onChangeCheck}
+              />{' '}
+              Urdú
+            </label>
+            <label className="check">
+              <input
+                type="checkbox"
+                name="ara"
+                data-group="languages"
+                onChange={onChangeCheck}
+              />{' '}
+              Àrab
+            </label>
+            <label className="check">
+              <input
+                type="checkbox"
+                name="chi"
+                data-group="languages"
+                onChange={onChangeCheck}
+              />{' '}
+              Xinès
+            </label>
+          </div>
+
+          <div className="checkgroup">
+            <label className="small">Coneixements</label>
+            <label className="check">
+              <input
+                type="checkbox"
+                name="win"
+                data-group="skills"
+                onChange={onChangeCheck}
+              />{' '}
+              Windows
+            </label>
+            <label className="check">
+              <input
+                type="checkbox"
+                name="mac"
+                data-group="skills"
+                onChange={onChangeCheck}
+              />{' '}
+              macOS
+            </label>
+            <label className="check">
+              <input
+                type="checkbox"
+                name="and"
+                data-group="skills"
+                onChange={onChangeCheck}
+              />{' '}
+              Android
+            </label>
+            <label className="check">
+              <input
+                type="checkbox"
+                name="ios"
+                data-group="skills"
+                onChange={onChangeCheck}
+              />{' '}
+              iOS
+            </label>
+            <label className="check">
+              <input
+                type="checkbox"
+                name="tvs"
+                data-group="skills"
+                onChange={onChangeCheck}
+              />{' '}
+              Televisors
+            </label>
+            <label className="check">
+              <input
+                type="checkbox"
+                name="pho"
+                data-group="skills"
+                onChange={onChangeCheck}
+              />{' '}
+              Fotografia
+            </label>
+          </div>
 
           <label>
             <input type="checkbox" name="conditions" required /> Acceptes les{' '}
