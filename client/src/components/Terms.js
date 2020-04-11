c1cefbf71bedb139d80522e0a16db065228a05ca
JuanIrache <yrache@gmail.com>
2020-04-11 12:18:06 +0200

    improve placeholder terms

--- client/src/components/Terms.js
+++ client/src/components/Terms.js
@@ -5,45 +5,183 @@ import '../style/Page.scss';
 export default () => (
   <div className="Page">
     <div className="container">
+      <h4>Informació general de la seu electrònica o lloc web nebots.cat.</h4>
       <p>
-        But I must explain to you how all this mistaken idea of denouncing
-        pleasure and praising pain was born and I will give you a complete
-        account of the system, and expound the actual teachings of the great
-        explorer of the truth, the master-builder of human happiness.
+        “nebots” (nebots.cat) és un domini de Juan Irache Duesca (en endavant,
+        el DESENVOLUPADOR) amb email app@prototyping.barcelona que aspira a
+        posar en contacte a persones amb dificultats per accedir a la tecnologia
+        amb voluntaris que els puguin ajudar.
+      </p>
+      <h4>Accés Usuaris</h4>
+      <p>
+        L’accés i/o ús del “nebots.cat” atribueix la condició d’usuari i implica
+        l’acceptació de les condicions d’accés i ús que consten en aquest avís
+        legal, així com la política de privacitat i dades personals. Les citades
+        condicions seran d’aplicació independentment de les condicions generals
+        de contractació i de comerç electrònic que en el seu cas resultin
+        d’aplicació en les funcionalitats que ofereixi el portal. L’usuari es
+        compromet a utilitzar els serveis i la informació facilitada al
+        “nebots.cat” sota criteris de bona fe.
       </p>
       <p>
-        No one rejects, dislikes, or avoids pleasure itself, because it is
-        pleasure, but because those who do not know how to pursue pleasure
-        rationally encounter consequences that are extremely painful. Nor again
-        is there anyone who loves or pursues or desires to obtain pain of
-        itself, because it is pain, but because occasionally circumstances occur
-        in which toil and pain can procure him some great pleasure.
+        L’accés al portal es gratuït. Cas de que en el futur existissin
+        funcionalitats o serveis subjectes al pagament d’un preu, es farien
+        constar les condicions d’ús.
       </p>
       <p>
-        To take a trivial example, which of us ever undertakes laborious
-        physical exercise, except to obtain some advantage from it? But who has
-        any right to find fault with a man who chooses to enjoy a pleasure that
-        has no annoying consequences, or one who avoids a pain that produces no
-        resultant pleasure?
+        En cas que per utilitzar determinades funcions o serveis es precisi el
+        registre de l’usuari, aquest s’efectuarà mitjançant formularis de
+        recapta de dades i amb subjecció a les prevencions de la legislació de
+        protecció de dades personals que es descriuen en aquest avis legal.
+      </p>
+      <p>Protecció dels drets de propietat intel·lectual i industrial</p>
+      <p>
+        Son titularitat del DESENVOLUPADOR els drets de Propietat Intel·lectual
+        i Industrial del “nebots.cat”, que inclou els continguts i elements
+        gràfics, el seu codi font, disseny, estructura de navegació, bases de
+        dades i altres elements que formen part del lloc web. Per tant,
+        correspon en exclusiva al DESENVOLUPADOR l’exercici, en exclusiva, dels
+        drets d’explotació en qualsevol forma i, en concret, dels drets de
+        reproducció, distribució, comunicació pública i transformació. Qualsevol
+        ús o reproducció que els usuaris facin d’aquests continguts, o d’altres
+        que es puguin incloure en el futur, s’han de fer seguint les previsions
+        i respectant les limitacions de la normativa de Propietat Intel·lectual
+        i en qualsevol cas amb l’autorització del DESENVOLUPADOR, sense
+        perjudici dels límits que estableix el Reial Decret 1/1996, del 12
+        d’abril, pel qual s’aprova el text refós de la Llei de propietat
+        intel·lectual .
+      </p>
+      <h4>Reserva de drets</h4>
+      <p>
+        El DESENVOLUPADOR explicita una reserva de drets amb caràcter general
+        respecte del conjunt dels continguts difosos en el lloc web que són
+        objecte de propietat intel·lectual i industrial. D’acord amb el previst
+        a l’article 32 del test refós de la Llei de Propietat intel·lectual, el
+        DESENVOLUPADOR s’oposa expressament a la utilització de qualsevol dels
+        continguts de “nebots.cat” en qualsevol format, amb la finalitat de fer
+        ressenyes o revistes de premsa amb finalitat comercial, sense la prèvia
+        autorització expressa del DESENVOLUPADOR.
       </p>
       <p>
-        On the other hand, we denounce with righteous indignation and dislike
-        men who are so beguiled and demoralized by the charms of pleasure of the
-        moment, so blinded by desire, that they cannot foresee the pain and
-        trouble that are bound to ensue; and equal blame belongs to those who
-        fail in their duty through weakness of will, which is the same as saying
-        through shrinking from toil and pain.
+        Aquest avís legal no suposa cap cessió de drets a favor de l’usuari en
+        relació a cap dels elements integrants de la web o dels seus continguts.
+      </p>
+      <p>No s’autoritza en cap cas:</p>
+      <ul>
+        <li>
+          L’extracció d’elements del lloc web que causi perjudici al
+          DESENVOLUPADOR, conforme a les disposicions vigents.
+        </li>
+        <li>L’ús comercial dels continguts del lloc web “nebots.cat”.</li>
+        <li>
+          L’ús de marques o signes distintius, logotips, combinacions de colors,
+          estructura, disseny i en general símbols distintius de qualsevol
+          naturalesa, propietat del DESENVOLUPADOR, sense el coneixement i
+          l’autorització corresponent del DESENVOLUPADOR.
+        </li>
+        <li>
+          L’ús de continguts en activitats il·lícites, il·legals o contràries a
+          la bona fe i l’ordre públic.
+        </li>
+        <li>
+          Difondre continguts o propaganda de caràcter racista o xenòfob,
+          pornogràfic, il·legal o que atempti contra els drets humans.
+        </li>
+        <li>
+          Provocar danys en els sistemes físics i lògics del DESENVOLUPADOR,
+          dels seus proveïdors o de terceres persones; introduir o difondre a la
+          xarxa virus informàtics o altres sistemes físics o lògics que siguin
+          susceptibles de provocar danys anteriorment esmentats.
+        </li>
+        <li>
+          Intentar accedir i, en el seu cas, utilitzar els comptes de correu
+          electrònic d’altres usuaris i modificar o manipular els seus
+          missatges.
+        </li>
+        <li>
+          L’usuari podrà visualitzar els continguts d’aquest portal, copiar-los,
+          imprimir-los en el seu ordenador o qualsevol altre suport amb destí
+          exclusiu al seu us personal i privat.
+        </li>
+      </ul>
+      <h4>Protecció de dades de caràcter personal</h4>
+      <p>Dades recollides a través de formularis</p>
+      <p>
+        El DESENVOLUPADOR, com a responsable del lloc web, de conformitat amb el
+        que disposa el Reglament (UE) 2016/679, de 27 d’abril, relatiu a la
+        protecció de les persones físiques pel que fa al tractament de dades
+        personals (en endavant, «RGPD»), i la Llei 34/2002, de 11 de juliol, de
+        serveis de la societat de la informació i del comerç electrònic
+        (LSSICE), li informa que ha implantat les mesures de seguretat
+        necessàries, d’índole tècnica i organitzativa , per garantir i protegir
+        la confidencialitat, la integritat i la disponibilitat de les dades
+        introduïdes.
+      </p>
+      <p>Responsabilitat amb relació als continguts</p>
+      <p>
+        el DESENVOLUPADOR es reserva el dret de modificar els continguts del web
+        i d’eliminar-los, així com també de limitar o impedir l’accés ja sigui
+        temporalment o definitiva, sense notificació prèvia.
       </p>
       <p>
-        These cases are perfectly simple and easy to distinguish. In a free
-        hour, when our power of choice is untrammelled and when nothing prevents
-        our being able to do what we like best, every pleasure is to be welcomed
-        and every pain avoided. But in certain circumstances and owing to the
-        claims of duty or the obligations of business it will frequently occur
-        that pleasures have to be repudiated and annoyances accepted. The wise
-        man therefore always holds in these matters to this principle of
-        selection: he rejects pleasures to secure other greater pleasures, or
-        else he endures pains to avoid worse pains.
+        el DESENVOLUPADOR no es fa responsable de l’accés tècnic, la informació
+        o dels continguts d’altres pàgines web a les quals remet o amb les quals
+        es pugui enllaçar des del portal “nebots.cat”.
+      </p>
+      <p>
+        el DESENVOLUPADOR no s’identifica necessàriament amb les opinions
+        expressades pels seus col·laboradors.
+      </p>
+      <p>
+        el DESENVOLUPADOR no es fa responsable dels actes, activitats i
+        esdeveniments organitzats per tercers dels quals s’informi al portal
+        “nebots.cat”, ni dels continguts de tercers (en especial seccions casos
+        d’èxit i vídeos).
+      </p>
+      <h4>Dret d’exclusió</h4>
+      <p>
+        el DESENVOLUPADOR es reserva el dret de denegar o retirar l’accés als
+        seus websites i/o els serveis oferts sense necessitat d’avís previ, a
+        instancia pròpia o d’un tercer, a aquells usuaris que incompleixin les
+        presents condicions generals.
+      </p>
+      <h4>Subscripcions</h4>
+      <p>
+        La subscripció a productes que es facin a traves d’aquest portal
+        “nebots.cat” serà voluntària i suposarà l’acceptació de les condicions
+        que en cada cas s’estableixin i que l’usuari haurà d’acceptar
+        expressament. També implicarà l’acceptació per a rebre informació
+        comercial de productes i serveis del DESENVOLUPADOR. El fet de donar-se
+        d’alta en aquests serveis comporta l’autorització al DESENVOLUPADOR per
+        a aquest tipus de comunicacions, podent l’usuari revocar l’autorització
+        amb una comunicació per escrit en la forma que el DESENVOLUPADOR
+        indicarà en cada cas.
+      </p>
+      <h4>Generalitats</h4>
+      <p>
+        El DESENVOLUPADOR perseguirà l’incompliment de les presents condicions
+        així com qualsevol utilització indeguda de les seves websites exercint
+        totes les accions civils i penals que li puguin correspondre per dret.
+      </p>
+      <p>
+        El DESENVOLUPADOR no pot garantir que el portal i el servidor estiguin
+        lliures de virus pel que no es fa responsable dels danys o perjudicis
+        que es puguin causar per aquestes anomalies o qualsevol altra d’indole
+        tècnica.
+      </p>
+      <h4>Modificació de les presents condicions i durada</h4>
+      <p>
+        El DESENVOLUPADOR podrà modificar en qualsevol moment les condicions
+        aquí determinades, essent degudament publicades com aquí apareixen. La
+        vigència de les citades condicions anirà en funció de la seva exposició
+        i estaran vigents fins que siguin modificades per altres degudament
+        publicades.
+      </p>
+      <h4>Legislació aplicable i jurisdicció</h4>
+      <p>
+        La relació entre el DESENVOLUPADOR i els usuaris es regirà per la
+        normativa espanyola vigent i qualsevol controvèrsia se sotmetrà als
+        jutjats i tribunals de la ciutat de Barcelona.
       </p>
     </div>
   </div>
