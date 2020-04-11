d72b2445d7a8dd28a6f5c98d6c0cc45e54a7f174
JuanIrache <yrache@gmail.com>
2020-04-11 16:02:12 +0200

    use html phone

--- client/src/components/Clients.js
+++ client/src/components/Clients.js
@@ -7,7 +7,9 @@ export default () => (
   <div className="Client">
     <div className="client-content">
       Truca al
-      <div className="number">{process.env.REACT_APP_PHONE}</div>
+      <a href={`tel:+34${process.env.REACT_APP_PHONE}`} className="number">
+        {process.env.REACT_APP_PHONE}
+      </a>
     </div>
     <Link to="/about">
       <h4>Com funciona?</h4>
