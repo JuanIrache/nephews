8048e75bf14f5cb672254f777fe897fb1085f912
Juan Irache <yrache@gmail.com>
2020-04-25 13:25:59 -0400

    only process ringing calls

--- server/routes/postCall.js
+++ server/routes/postCall.js
@@ -10,20 +10,23 @@ const { NEBOTS_SERVER, NEBOTS_CLIENTURL } = process.env;
 
 module.exports = async (req, res) => {
   try {
-    // Prepare response for Twilio
-    const twiml = new VoiceResponse();
-    // Play welcome message
-    twiml.play(NEBOTS_CLIENTURL + '/gracies.mp3');
+    if (req.body.CallStatus === 'ringing') {
+      // Prepare response for Twilio
+      const twiml = new VoiceResponse();
+      // Play welcome message
+      twiml.play(NEBOTS_CLIENTURL + '/gracies.mp3');
 
-    // Record user question
-    twiml.record({
-      transcribeCallback: NEBOTS_SERVER + '/transcription'
-    });
+      // Record user question
+      twiml.record({
+        transcribeCallback: NEBOTS_SERVER + '/transcription'
+      });
 
-    // Render the response as XML in reply to the webhook request
-    res.type('text/xml');
-    console.log(`${getDate()} - Call started`);
-    return res.send(twiml.toString());
+      // Render the response as XML in reply to the webhook request
+      res.type('text/xml');
+      console.log(`${getDate()} - Call started`);
+      return res.send(twiml.toString());
+    }
+    return res.sendStatus(200);
   } catch (error) {
     console.error(`${getDate()} - Error handling Call: ${error.message}`);
     return res.sendStatus(500);
